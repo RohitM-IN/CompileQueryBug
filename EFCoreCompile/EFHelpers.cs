4ca2a1751705809eb4536b0fe08ee771a49d67ca
RohitM-IN <rohit.mahajan.in@gmail.com>
2025-08-12 09:52:44 +0530

    first commit

---
+++ EFCoreCompile/EFHelpers.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Concurrent;
+using System.Linq.Expressions;
+
+namespace EFCoreCompile
+{
+    public static class EFWrap
+    {
+        // Each overload gets its own typed cache to avoid boxing/casting issues
+        private static readonly ConcurrentDictionary<(object model, LambdaExpression expr), object> _cache = new();
+
+        // 0 parameters
+        public static Func<TContext, TResult> CompileQuery<TContext, TResult>(
+            Expression<Func<TContext, TResult>> query)
+            where TContext : DbContext
+        {
+            return ctx =>
+            {
+                var key = (ctx.Model, (LambdaExpression)query);
+                var compiled = (Func<TContext, TResult>)_cache.GetOrAdd(
+                    key, _ => EF.CompileQuery(query));
+                return compiled(ctx);
+            };
+        }
+
+        // 1 parameter
+        public static Func<TContext, TParam1, TResult> CompileQuery<TContext, TParam1, TResult>(
+            Expression<Func<TContext, TParam1, TResult>> query)
+            where TContext : DbContext
+        {
+            return (ctx, p1) =>
+            {
+                var key = (ctx.Model, (LambdaExpression)query);
+                var compiled = (Func<TContext, TParam1, TResult>)_cache.GetOrAdd(
+                    key, _ => EF.CompileQuery(query));
+                return compiled(ctx, p1);
+            };
+        }
+
+        // 2 parameters
+        public static Func<TContext, TParam1, TParam2, TResult> CompileQuery<TContext, TParam1, TParam2, TResult>(
+            Expression<Func<TContext, TParam1, TParam2, TResult>> query)
+            where TContext : DbContext
+        {
+            return (ctx, p1, p2) =>
+            {
+                var key = (ctx.Model, (LambdaExpression)query);
+                var compiled = (Func<TContext, TParam1, TParam2, TResult>)_cache.GetOrAdd(
+                    key, _ => EF.CompileQuery(query));
+                return compiled(ctx, p1, p2);
+            };
+        }
+    }
+}
---
+++ EFCoreCompile/Program.cs
@@ -0,0 +1,63 @@
+﻿using EFCoreCompile;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    // Compiled query: Get all blogs ordered by BlogId
+    private static readonly Func<BloggingContext, IEnumerable<Blog>> GetAllBlogs =
+        EFWrap.CompileQuery(
+            (BloggingContext ctx) =>
+                ctx.Blogs
+                   .Where(b => b.BlogId > 1)
+            );
+
+    static void Main()
+    {
+        using var db = new BloggingContext();
+
+        Console.WriteLine($"Database path: {db.DbPath}.");
+
+        // Create sample blogs
+        Console.WriteLine("Inserting blogs");
+        db.AddRange(
+            new Blog { Url = "http://blogs.msdn.com/adonet" },
+            new Blog { Url = "https://devblogs.microsoft.com/dotnet" }
+        );
+        db.SaveChanges();
+
+        // Read list using compiled query
+        Console.WriteLine("Querying all blogs");
+        var blogs = GetAllBlogs(db).ToList(); // Materialize to List
+
+        foreach (var blog in blogs)
+        {
+            Console.WriteLine($"BlogId: {blog.BlogId}, Url: {blog.Url}");
+        }
+
+        // Update first blog
+        if (blogs.Any())
+        {
+            var firstBlog = blogs.First();
+            Console.WriteLine("Updating the first blog and adding a post");
+            firstBlog.Url = "https://dotnet.microsoft.com";
+            firstBlog.Posts.Add(new Post
+            {
+                Title = "Hello World",
+                Content = "I wrote an app using EF Core!"
+            });
+            db.SaveChanges();
+        }
+
+        // Delete last blog
+        if (blogs.Any())
+        {
+            var lastBlog = blogs.Last();
+            Console.WriteLine("Deleting last blog");
+            db.Remove(lastBlog);
+            db.SaveChanges();
+        }
+    }
+}
